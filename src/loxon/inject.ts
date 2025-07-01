148854cd897cc77ee7b1a31d5b07e39c6a535223
darrell <darrell.anderson@gmail.com>
2025-06-30 20:40:11 -0700

    progress

---
+++ src/loxon/inject.ts
@@ -0,0 +1,72 @@
+import { refPackageId } from "@tabletop-playground/api";
+import { HomebrewRegistry } from "ti4-ttpg-ts-types";
+
+new HomebrewRegistry().load({
+  sourceAndPackageId: {
+    source: "homebrew.loxon",
+    packageId: refPackageId,
+  },
+
+  factions: [
+    {
+      name: "Loxon Herd",
+      nsidName: "loxon",
+      abbr: "Loxon",
+      abilities: ["vigilant", "trampling", "imposing-stature"],
+      commodities: 3,
+      factionTechs: ["resolute-2", "universal-docking-protocols"],
+      home: 3001,
+      leaders: {
+        agents: ["shahm-kewr"],
+        commanders: ["daahzi-pohip"],
+        heroes: ["fractured-spyglass"],
+        mechs: ["surveyor"],
+      },
+      promissories: ["exotic-locale"],
+      startingTechs: ["duranium-arbor"],
+      startingUnits: { flagship: 1, destroyer: 2, infantry: 4, spaceDock: 1 },
+      unitOverrides: ["arill-queen", "resolute", "resolute-2"],
+      extras: [{ nsid: "token.attachment.planet:homebrew.loxon/loxon-a" }],
+    },
+  ],
+
+  systems: [
+    {
+      tile: 3001,
+      planets: [
+        { name: "Pachydra", nsidName: "pachydra", resources: 3, influence: 3 },
+        {
+          name: "Gomphyr",
+          nsidName: "gomphyr",
+          resources: 1,
+          influence: 1,
+        },
+      ],
+    },
+  ],
+
+  planetAttachments: [
+    { name: "Loxon A", nsidName: "loxon-a", traits: ["cultural"] },
+  ],
+
+  unitAttrs: [
+    {
+      unit: "flagship",
+      name: "Arill Queen",
+      nsidName: "arill-queen",
+      spaceCombat: { hit: 9, dice: 2 },
+    },
+    {
+      unit: "destroyer",
+      name: "Resolute",
+      nsidName: "resolute",
+      hasSustainDamage: true,
+    },
+    {
+      unit: "destroyer",
+      name: "Resolute 2",
+      nsidName: "resolute-2",
+      hasSustainDamage: true,
+    },
+  ],
+});
